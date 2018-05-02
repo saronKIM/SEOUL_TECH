b84f07c68c67b8c4c89769bdc79477d8b9439749
stc_176-PC\stc_176 <rlatkfhs1@gmail.com>
2018-05-02 10:59:53 +0900

    합계부분 삭제

---
+++ LEDonoff/GccApplication1/GccApplication1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 2018-05-02 오전 10:52:06
+ * Author : stc_176
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+int main(void)
+{
+	PORTD=0xff;
+	DDRD=0xff;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTD=0xff;
+		_delay_ms(600);
+		PORTD=0x00;
+		_delay_ms(500);
+    }
+}
+
